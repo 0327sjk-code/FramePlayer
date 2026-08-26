db50c20871b440be9b57dfc2ef8cc34452877fee
0327sjk-code <228685116+0327sjk-code@users.noreply.github.com>
2026-08-26 10:29:54 +0800

    Release FramePlayer v0.2.0 with online updates

--- src/App/ProductInfo.h
+++ src/App/ProductInfo.h
@@ -4,7 +4,7 @@ namespace zt::sequence::product {
 
 inline constexpr wchar_t Name[] = L"FramePlayer";
 inline constexpr wchar_t DisplayName[] = L"序列播放器";
-inline constexpr wchar_t Version[] = L"0.1.0";
+inline constexpr wchar_t Version[] = L"0.2.0";
 inline constexpr wchar_t GitHubRepository[] = L"0327sjk-code/FramePlayer";
 inline constexpr wchar_t UpdateVersionUrl[] =
     L"https://github.com/0327sjk-code/FramePlayer/releases/latest/download/version.txt";
